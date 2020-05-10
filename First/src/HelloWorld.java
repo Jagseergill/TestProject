7eff110f79fde67b7d4eae49a776f4cfa1c53ef8
Jagseer Singh <Jagseer Singh@DESKTOP-D8B02MI>
2020-05-10 11:16:45 -0700

    First

---
+++ First/src/HelloWorld.java
@@ -0,0 +1,11 @@
+import javax.swing.JOptionPane;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+
+	System.out.println("Hello Welcome to the java program");
+	//	JOptionPane.showMessageDialog(null, "Welcome to java");
+	}
+
+}
