3fc7695bd1e59dfb0125070de1558acddef42bd7
Jagseer Singh <Jagseer Singh@DESKTOP-D8B02MI>
2020-05-10 12:39:26 -0700

    Data

---
+++ Data/src/Variable.java
@@ -0,0 +1,11 @@
+
+public class Variable {
+
+	public static void main(String[] args) {
+		int billingDate = 5;
+		System.out.print("Bill are sent on day ");
+		System.out.print(billingDate);
+		System.out.println(" of the month");
+		System.out.println("Next bill: June " + billingDate);
+	}
+}
---
+++ Data/src/VariableTwo.java
@@ -0,0 +1,9 @@
+
+public class VariableTwo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
